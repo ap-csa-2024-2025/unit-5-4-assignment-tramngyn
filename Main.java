8490e6c8197fb867ef63567c447311ec0ca2ff93
tramngyn <bvnguyen115@0student.hbuhsd.edu>
2024-09-23 11:19:38 -0700

    finished homework

--- Main.java
+++ Main.java
@@ -2,12 +2,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    double someDist = distance(0, 5, 0, 0);
+    System.out.println(someDist);
+
+    System.out.println();
+    
+    double someNum = quadForm(1,8,16);
+    System.out.println(someNum);
   }
 
-  // Implement distance formula method here
+  public static double distance(double x1, double y1, double x2, double y2) // Implement distance formula method here
+  {
+    return Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+  }
 
-  // implement one of your quadratic formula methods here
-
-  // implement the other quadratic formula here
+  public static double quadForm(double a, double b, double c)
+  {
+    return (-b+Math.sqrt((b*b)-4*a*c))/2*a;
+  }
 }
